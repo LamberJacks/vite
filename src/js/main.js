7d225cf4e833c485fa49f7e8ee5b5c10b919f3bf
LamberJacks <ivzet009@gmail.com>
2026-03-11 11:39:11 +0300

    init project + mixins

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import '../scss/main.scss';
+import { setupCounter } from './counter.js';
+import { foo } from './counter.js';
+
+console.log(foo);
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
