6d36844cadcb4b744ce910f94c15837e907eadea
LamberJacks <ivzet009@gmail.com>
2026-03-12 17:25:16 +0300

    add basis styles

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  root: "./src",
+  root: './src',
+
   server: {
-    port: 3000, 
+    port: 3000,
     open: true,
   },
+
+  plugins: [
+    injectHTML(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpg: { quality: 80 },
+    }),
+  ],
+
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
+    },
   },
-})
+});
